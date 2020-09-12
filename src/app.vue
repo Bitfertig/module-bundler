80b8e60c422f74b06b7977fa2afb70f18d4ec697
Aurelian Hermand <aurel@hermand.de>
2020-09-12 18:30:19 +0200

    app transformation to vuejs and layout completed

--- src/app.vue
+++ src/app.vue
@@ -5,42 +5,81 @@
 
             <form @change="generate()">
             
-                <h1>webpack Generator</h1>
+                <h1><span class="inner">webpack configurator</span></h1>
 
                 <div class="box">
-                    Target:
-                    <select name="target" id="target" v-model="target">
-                        <option value="web">web</option>
-                        <option value="node">node</option>
-                    </select>
+                    <h2>Mode</h2>
+                    <div class="content">
+                        <select name="mode" id="mode" v-model="mode">
+                            <option value=""></option>
+                            <option value="development">development</option>
+                            <option value="production">production</option>
+                            <option value="none">none</option>
+                        </select>
+                        <div>
+                            <small>
+                                Mode, that tells webpack to use its built-in optimizations accordingly.
+                                See <a href="https://webpack.js.org/configuration/mode/" target="_blank">mode</a>.
+                            </small>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="box">
-                    Entry: <input type="text" name="entry" id="entry" v-model="entry">
+                    <h2>Target</h2>
+                    <div class="content">
+                        <select name="target" id="target" v-model="target">
+                            <option value="async-node">async-node</option>
+                            <option value="electron-main">electron-main</option>
+                            <option value="electron-renderer">electron-renderer</option>
+                            <option value="electron-preload">electron-preload</option>
+                            <option value="node">node</option>
+                            <option value="node-webkit">node-webkit</option>
+                            <option value="web">web (default)</option>
+                            <option value="webworker">webworker</option>
+                        </select>
+                        <div>
+                            <small>
+                                Compile for multiple environments or targets.
+                                See <a href="https://webpack.js.org/configuration/target/" target="_blank">target</a>.
+                            </small>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="box">
-                    Output path:
-                    <input type="radio" name="output_path_type" id="output_path_type_function" value="function" v-model="output_path_type"> <input type="text" id="output_path_function" v-model="output_path_function">
-                    <input type="radio" name="output_path_type" id="output_path_type_string" value="string" v-model="output_path_type"> <input type="text" id="output_path_string" v-model="output_path_string">
+                    <h2>Entry</h2>
+                    <div class="content">
+                        <input type="text" name="entry" id="entry" v-model="entry">
+                    </div>
                 </div>
 
                 <div class="box">
-                    Output filename: <input type="text" id="output_filename" v-model="output_filename">
+                    <h2>Output</h2>
+                    <div class="content">
+                        Path:
+                        <input type="radio" name="output_path_type" id="output_path_type_function" value="function" v-model="output_path_type"> <input type="text" id="output_path_function" v-model="output_path_function">
+                        <input type="radio" name="output_path_type" id="output_path_type_string" value="string" v-model="output_path_type"> <input type="text" id="output_path_string" v-model="output_path_string">
+                        <br>
+                        Filename:
+                        <input type="text" id="output_filename" v-model="output_filename">
+                    </div>
                 </div>
 
                 <div class="box">
-                    Features
-                    <ul id="">
-                        <li><input type="checkbox" id="clean" value="1" v-model="features.clean"> <label for="clean">Cleanup</label></li>
-                        <li><input type="checkbox" id="html" value="1" v-model="features.html"> <label for="html">HTML</label></li>
-                        <li><input type="checkbox" id="css_extract" value="1" v-model="features.css_extract"> <label for="css_extract">CSS Extract</label></li>
-                        <li><input type="checkbox" id="css" value="1" v-model="features.css"> <label for="css">CSS</label></li>
-                        <li><input type="checkbox" id="sass" value="1" v-model="features.sass"> <label for="sass">SASS</label></li>
-                        <li><input type="checkbox" id="less" value="1" v-model="features.less"> <label for="less">Less</label></li>
-                        <li><input type="checkbox" id="babel" value="1" v-model="features.babel"> <label for="babel">Babel</label></li>
-                        <li><input type="checkbox" id="vue" value="1" v-model="features.vue"> <label for="vue">Vue</label></li>
-                    </ul>
+                    <h2>Features</h2>
+                    <div class="content">
+                        <ul id="">
+                            <li><input type="checkbox" id="clean" value="1" v-model="features.clean"> <label for="clean">Cleanup</label></li>
+                            <li><input type="checkbox" id="html" value="1" v-model="features.html"> <label for="html">HTML</label></li>
+                            <li><input type="checkbox" id="css_extract" value="1" v-model="features.css_extract"> <label for="css_extract">CSS Extract</label></li>
+                            <li><input type="checkbox" id="css" value="1" v-model="features.css"> <label for="css">CSS</label></li>
+                            <li><input type="checkbox" id="sass" value="1" v-model="features.sass"> <label for="sass">SASS</label></li>
+                            <li><input type="checkbox" id="less" value="1" v-model="features.less"> <label for="less">Less</label></li>
+                            <li><input type="checkbox" id="babel" value="1" v-model="features.babel"> <label for="babel">Babel</label></li>
+                            <li><input type="checkbox" id="vue" value="1" v-model="features.vue"> <label for="vue">Vue</label></li>
+                        </ul>
+                    </div>
                 </div>
 
             </form>
@@ -56,9 +95,7 @@
 
         <div class="out">
 
-            webpack.config.js
-            <!-- <highlightjs language='javascript' code="var x = 5;" /> -->
-            <pre><code v-html="webpack_config"></code></pre>
+            <pre><code class="hljs language-javascript" v-html="webpack_config"></code></pre>
 
         </div>
 
@@ -67,10 +104,10 @@
 
 <script>
 
-import hljs from 'highlight.js'; // all languages
-//import hljs from 'highlight.js/lib/core';
-//import javascript from 'highlight.js/lib/languages/javascript';
-//hljs.registerLanguage('javascript', javascript);
+//import hljs from 'highlight.js'; // all languages
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+hljs.registerLanguage('javascript', javascript);
 import 'highlight.js/scss/vs2015.scss';
 //import 'highlight.js/scss/atom-one-dark.scss';
 
@@ -82,13 +119,14 @@ export default {
     },
     data: function() {
         return {
+            mode: '',
             target: 'web',
             entry: 'src/index.js',
             output_filename: 'bundle.js',
-            output_path_type: 'function', // document.querySelector('[name=output_path_type]:checked').value;
+            output_path_type: 'function',
             output_path_function: `path.resolve(__dirname, 'dist')`, 
             output_path_string: './dist',
-            output_filename: 'bundle.js',// document.querySelector('#output_filename').value;
+            output_filename: 'bundle.js',
             features: {
                 clean: false,
                 html: false,
@@ -100,16 +138,12 @@ export default {
                 vue: false,
             },
 
-            //devInstalls: [],
-            //requires: [],
-            //rules: [],
-            //plugins: [],
-
             webpack_config: '',
         }
     },
     mounted () {
         console.log('mounted');
+        this.generate();
     },
     computed: {
         output_path: function() {
@@ -128,9 +162,10 @@ export default {
         generate: function(){
             console.log('generate');
 
-
-            var devInstalls = ['webpack', 'webpack-cli']
             var requires = [];
+            var devInstalls = ['webpack', 'webpack-cli'];
+            var installs = [];
+            var mode = '';//this.mode;
             var target = this.target;
             var entry = this.entry;
             var output_path = this.output_path;
@@ -138,6 +173,14 @@ export default {
             var rules = [];
             var plugins = [];
 
+
+            if ( this.mode != '' ) {
+                let mode_comment = '';
+                if ( this.mode == 'development' ) mode_comment += ' // Sets process.env.NODE_ENV on DefinePlugin to value development. Enables NamedChunksPlugin and NamedModulesPlugin.';
+                if ( this.mode == 'production' ) mode_comment += ' // Sets process.env.NODE_ENV on DefinePlugin to value production. Enables FlagDependencyUsagePlugin, FlagIncludedChunksPlugin, ModuleConcatenationPlugin, NoEmitOnErrorsPlugin, OccurrenceOrderPlugin, SideEffectsFlagPlugin and TerserPlugin.';
+                if ( this.mode == 'none' ) mode_comment += ' // Opts out of any default optimization options.';
+                mode = `mode: '${this.mode}',${mode_comment}`;
+            }
 
             if ( this.features.clean ) {
                 devInstalls.push('clean-webpack-plugin');
@@ -181,7 +224,7 @@ export default {
                 devInstalls.push('css-loader', 'sass-loader', 'node-sass', 'style-loader');
                 rules.push(`
                     {
-                        test: /\.scss$/,
+                        test: /\.s[ac]ss$/,
                         use: [
                             'style-loader',
                             'css-loader',
@@ -231,7 +274,7 @@ export default {
 
             if ( this.features.vue ) {
                 devInstalls.push('vue-loader', 'vue-template-compiler');
-                //installs.push('npm install vue'); // TODO:
+                installs.push('vue');
                 requires.push(`const VueLoaderPlugin = require('vue-loader/lib/plugin');`);
                 rules.push(`
                     {
@@ -245,19 +288,22 @@ export default {
             }
 
     
-            devInstalls = devInstalls.filter((x, i, a) => a.indexOf(x) == i).join(' ');
-            requires = requires.map(item => item.trim('')).join("\n");
-            rules = rules.map(item => item.trim('')).join("\n").trim('');
-            plugins = plugins.map(item => item.trim('')).join("\n").trim('');
+            devInstalls = this.array_unique(devInstalls).join(' ');
+            installs = this.array_unique(installs).join(' ');
+            requires = this.map_trim(requires).join("\n");
+            rules = this.map_trim(rules).join("\n");//.trim('');
+            plugins = this.map_trim(plugins).join("\n");//.trim('');
 
 
             var webpack_config = `
 /**
- * Webpack configuration
+ * webpack configuration
+ * File: webpack.config.js
  * 
  * Installs:
  * npm init -y
  * npm install --save-dev ${devInstalls}
+ * npm install ${installs}
  */
 
 const webpack = require('webpack');
@@ -265,6 +311,7 @@ const path = require('path');
 ${requires}
 
 const config = {
+    ${mode}
     target: '${target}',
     entry: {
         main: '${entry}'
@@ -287,18 +334,25 @@ const config = {
 module.exports = config;
 `;
 
-            webpack_config = webpack_config.trim();
 
-            webpack_config = beautify(webpack_config, { indent_size: 2, space_in_empty_paren: true });
+            webpack_config = beautify(webpack_config, { indent_size: 2, space_in_empty_paren: true }); // Options: https://www.npmjs.com/package/js-beautify#options https://beautifier.io/
             webpack_config = hljs.highlightAuto( webpack_config ).value;
 
             this.webpack_config = webpack_config;
 
         },
+
+        // Helpers
+        array_unique: function(arr) {
+            return arr.filter((x, i, a) => a.indexOf(x) == i);
+        },
+        map_trim: function(arr) {
+            return arr.map(item => item.trim(''));
+        }
     },
 }
 </script>
 
 <style>
-body {border:10px solid red;}
+/* body {border:10px solid red;} */
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ const config = {
                 ]
             }, */
             {
-                test: /\.scss$/,
+                test: /\.s[ac]ss$/,
                 use: [
                     'style-loader',
                     'css-loader',
